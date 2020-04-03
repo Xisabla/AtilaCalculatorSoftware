01a81349825785151705cc88da1130a1b8b02ab6
jeanjeandelastreet <jean.frenoi59@laposte.net>
2020-04-03 13:29:11 +0200

    jeanmodif

--- qt/mainwindow.cpp
+++ qt/mainwindow.cpp
@@ -7,15 +7,13 @@
 #include <vtkPointData.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
-#include <vtkSmartPointer.h>
-#include <vtkUnsignedCharArray.h>
-#include <vtkVertexGlyphFilter.h>
 #include <vtkPolyData.h>
 #include <vtkLookupTable.h>
 #include <vtkFloatArray.h>
-#include "binary_data_GiD.hpp"
 
-MainWindow::MainWindow(char *c)
+
+
+MainWindow::MainWindow(char *c )
 {
     this->setupUi(this);
     Str_binary_data_GiD binary_data(string( string(c) + string("/Bar-Thermal-3034bSSE3-Pascal.flavia.res")));
@@ -23,46 +21,29 @@ MainWindow::MainWindow(char *c)
     while (auto one_result = binary_data.read_one_result()) {
 		binary_data.results_.emplace_back(std::move(*one_result));
 	  }
-    auto points = vtkSmartPointer<vtkPoints>::New();
-    int nnode = 0; 
-    for (auto &&mesh : binary_data.meshes_)
-    {
-      for (auto &&nodes : mesh.tab_of_nodes_)
-      {
-        points->InsertNextPoint(nodes.coord[0],nodes.coord[1],nodes.coord[2]);
-        nnode++;
-      }
-      cout<<"Element is "<<mesh.elements_[10]<<endl;
-    }    
-    auto pointsPolydata = vtkSmartPointer<vtkPolyData>::New();
-    pointsPolydata->SetPoints(points);
-    
-    auto vertexFilter = vtkSmartPointer<vtkVertexGlyphFilter>::New();
-    
+    auto [points , triangles , nb_points] = this->createTrianglesFromMeshCoordinates(binary_data);
 
     vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
     qvtkWidget->setRenderWindow(renderWindow);
 
     auto scalars = vtkSmartPointer<vtkFloatArray>::New();
-    scalars->SetNumberOfValues( nnode );
-    for (float i = 0; i < nnode; i++)
+    scalars->SetNumberOfValues( nb_points );
+    for (float i = 0; i < nb_points; i++)
     {
-      scalars->SetValue(i,static_cast<float>(i)/nnode);
+      scalars->SetValue(i,static_cast<float>(i)/nb_points);
     }
 
     auto colors = vtkSmartPointer<vtkNamedColors>::New();
     auto lut = vtkSmartPointer<vtkLookupTable>::New();
-    cout<<"Numberofnode is "<<nnode<<endl;
-    lut->SetNumberOfTableValues(nnode + 1 );
+    lut->SetNumberOfTableValues(nb_points + 1 );
     lut->Build();
-
-    pointsPolydata->GetPointData()->SetScalars(scalars);
-    vertexFilter->SetInputData(pointsPolydata);
-    vertexFilter->Update();
     
-  // Create a polydata object
+    // Create a polydata object
     auto polyData = vtkSmartPointer<vtkPolyData>::New();
-    polyData->ShallowCopy(vertexFilter->GetOutput());
+
+    polyData->SetPoints(points);
+    polyData->SetPolys(triangles);
+    polyData->GetPointData()->SetScalars(scalars);
 
     auto polyMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
     polyMapper->SetInputData(polyData);
@@ -73,7 +54,7 @@ MainWindow::MainWindow(char *c)
     polyActor->SetMapper(polyMapper);
     polyActor->GetProperty()->SetPointSize(2);
 
-  // VTK Renderer
+    // VTK Renderer
     auto renderer = vtkSmartPointer<vtkRenderer>::New();
     renderer->AddActor(polyActor);
     renderer->SetBackground(colors->GetColor3d("SteelBlue").GetData());
@@ -83,7 +64,39 @@ MainWindow::MainWindow(char *c)
 
     connect(this->actionExit, SIGNAL(triggered()), this, SLOT(slotExit()));
 }
-
+std::tuple< vtkSmartPointer<vtkPoints> , vtkSmartPointer<vtkCellArray> ,const int > MainWindow::createTrianglesFromMeshCoordinates(Str_binary_data_GiD &binary_data){
+    auto points = vtkSmartPointer<vtkPoints>::New();
+    auto triangles = vtkSmartPointer<vtkCellArray>::New();
+    int nb_points = 0; 
+    for (auto &&mesh : binary_data.meshes_)
+    {
+      for (auto &&nodes : mesh.tab_of_nodes_)
+      {
+        points->InsertNextPoint(nodes.coord[0],nodes.coord[1],nodes.coord[2]);
+        nb_points++;
+      }
+      for(auto i= 0 ; i<mesh.nb_of_elements_;i++){
+        auto[id, element] = mesh.get_an_element(i);
+        cout<<" i "<< i << " id "<< id << endl;
+        if(id == i+1){
+          auto triangle = vtkSmartPointer<vtkTriangle>::New();
+          triangle->GetPointIds()->SetId ( 0, element[0]--);
+          triangle->GetPointIds()->SetId ( 1, element[1]--);
+          triangle->GetPointIds()->SetId ( 2, element[2]--);
+          cout<<"id "<<id<<" element "<<element[0]<<endl;
+          triangles->InsertNextCell ( triangle );
+          }
+        else
+        {
+          cout<<"i et id ne correspondent pas "<<endl;
+          break;
+        }
+        
+      }
+    } 
+    return std::make_tuple(points,triangles,nb_points);
+    throw std::invalid_argument("binary data");   
+}
 MainWindow::~MainWindow()
 {
     delete this;
--- qt/mainwindow.h
+++ qt/mainwindow.h
@@ -1,20 +1,27 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <tuple>
 #include <QMainWindow>
 #include <QString>
+#include <vtkTriangle.h>
+#include <vtkCellArray.h>
+#include <vtkSmartPointer.h>
+
 #include "ui_mainwindow.h"
+#include "binary_data_GiD.hpp"
 
 
 class MainWindow : public QMainWindow , public Ui::MainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(char *c);
     ~MainWindow();
 public slots:
     void slotExit();
+private : 
+    std::tuple< vtkSmartPointer<vtkPoints> , vtkSmartPointer<vtkCellArray> ,const int > createTrianglesFromMeshCoordinates(Str_binary_data_GiD &binary_data);
 };
 
 #endif // MAINWINDOW_H
