23684bd9b872722f063d4b255ec6a70687684313
Gautier Miquet <xisabla.dev@gmail.com>
2020-11-23 13:56:36 +0100

    move headers to include dir and change CMakeLists strcture

---
+++ include/version.h
@@ -0,0 +1,21 @@
+/*=========================================================================
+
+  Project:   AtilaCalculatorSoftware
+  File:      version.h
+
+  Copyright (c) 2020 - All rights reserved
+  Distributed under the MIT License (https://opensource.org/licenses/MIT)
+
+=========================================================================*/
+#pragma once
+#ifndef ATILACALCULATORSOFTWARE_VERSION_H
+#define ATILACALCULATORSOFTWARE_VERSION_H
+
+#define ACS_VERSION_MAJOR 2
+#define ACS_VERSION_MINOR 0
+#define ACS_VERSION_PATCH 0
+
+#define LIB_CRYPTO_VERSION \
+    (ACS_VERSION_MAJOR * 10000 + (ACS_VERSION_MINOR) * 100 + (ACS_VERSION_PATCH)
+
+#endif // ATILACALCULATORSOFTWARE_VERSION_H
