451d0100b9138939020398e577017a36f388daa8
Gautier Miquet <xisabla.dev@gmail.com>
2020-11-26 11:50:16 +0100

    add missing script doc header

--- scripts/prepare-documentation.sh
+++ scripts/prepare-documentation.sh
@@ -1,3 +1,18 @@
+#
+# SCRIPT  prepared-documentation.sh
+# AUTHOR  Gautier MIQUET <xisabla.dev@gmail.com>
+# DATE:   2020-11-26
+# REV:    1.0.D (Valid are A, B, D, T and P)
+#               (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: GNU/Linux
+#
+# PURPOSE:  Remove all files that do not take place  on documentation branch
+#
+# USAGE:    ./prepared-documentation.sh
+#
+
+# Elements to remove
 REMOVE=(
   build
   cmake
